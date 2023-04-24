59c92af9537f8e130a52719707a668098b05e99f
Merveille Lubenzo <84930020+TekSniper@users.noreply.github.com>
2023-04-24 15:23:59 +0200

    Avancement du projet

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="static/css/main.css">
+    <link rel="stylesheet" href="static/css/node_modules/bulma/css/bulma.min.css">
+    <link rel="icon" href="static/img/favicon(1).ico" type="image/icon">
+    <title>Bienvenue</title>
+</head>
+<body class="body-index">
+    <div class="container">
+        <div class="columns is-centered" style="padding-top:100px">
+            <div class="column index-eputy">
+                <h2 class="" style="color:#fff; font-size:100px; font-weight:bolder">EPUTY</h2><br>
+                <a href="pages/authentification.php">
+            </div>
+        </div>            
+    </div>    
+</body>
+</html>
