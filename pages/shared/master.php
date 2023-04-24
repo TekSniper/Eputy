cb40448911aa291c35ac178ee3c4a3d9bdd3f1a1
Merveille Lubenzo <84930020+TekSniper@users.noreply.github.com>
2023-04-24 17:17:26 +0200

    Master -1

---
+++ pages/shared/master.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="static/css/main.css">
+    <link rel="stylesheet" href="static/css/node_modules/bulma/css/bulma.min.css">
+    <link rel="icon" href="static/img/Eputy Logo.ico" type="image/icon">
+    
+    <link href="static/css/fontawesome-free-6.3.0-web/css/all.css" rel="stylesheet" />
+    <link href="static/css/fontawesome-free-6.3.0-web/css/all.min.css" rel="stylesheet" />
+    <title>
+        Eputy App
+    </title>
+</head>
+<body>
+    
+</body>
+</html>
