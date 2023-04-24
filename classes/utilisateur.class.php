8206e3afb075795dab142ae50c9f04c4245c8c25
Merveille Lubenzo <84930020+TekSniper@users.noreply.github.com>
2023-04-24 16:26:04 +0200

    Projet - 2

---
+++ classes/utilisateur.class.php
@@ -0,0 +1,9 @@
+<?php
+class utilisateur
+{
+    public $nom;
+}
+
+
+
+?>
--- index.php
+++ index.php
@@ -6,15 +6,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="static/css/main.css">
     <link rel="stylesheet" href="static/css/node_modules/bulma/css/bulma.min.css">
-    <link rel="icon" href="static/img/favicon(1).ico" type="image/icon">
-    <title>Bienvenue</title>
+    <link rel="icon" href="static/img/Eputy Logo.ico" type="image/icon">
+    
+    <link href="static/css/fontawesome-free-6.3.0-web/css/all.css" rel="stylesheet" />
+    <link href="static/css/fontawesome-free-6.3.0-web/css/all.min.css" rel="stylesheet" />
+    <title>Accueil - Eputy</title>
 </head>
 <body class="body-index">
     <div class="container">
         <div class="columns is-centered" style="padding-top:100px">
             <div class="column index-eputy">
                 <h2 class="" style="color:#fff; font-size:100px; font-weight:bolder">EPUTY</h2><br>
-                <a href="pages/authentification.php">
+                <a href="pages/authentification.php" class="button is-danger is-outlined"><i class="fa-solid fa-user"></i> Se connecter !</a>
             </div>
         </div>            
     </div>    
