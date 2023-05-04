0e54330e23e06e97a5128f9b188b2ca6eeff57c8
Merveille Lubenzo <84930020+TekSniper@users.noreply.github.com>
2023-05-04 13:36:29 +0200

    Enregistrement candidat

--- pages/candidat-score.php
+++ pages/candidat-score.php
@@ -1,11 +1,21 @@
 <?php
 session_start();
+$error_message = "";
+$success_message = "";
+try {
+    $db = new PDO('mysql:host=localhost;dbname=eputy_base', 'root', '');
+} catch (Exception $e) {
+    die('Erreur : ' . $e->getMessage());
+}
+
 if ($_SESSION['profile'] == 'Administrateur') {
     include 'shared/header-admin.php';
 } else {
     include 'shared/header.php';
     header('Location: score.php');
 }
+
+$rep = $db->query("select * from pays");
 
 ?>
 
@@ -14,13 +24,13 @@ if ($_SESSION['profile'] == 'Administrateur') {
         <h3 class="title" style="color:#1C3D59; margin-top:25px">
             Identification du Candidat
         </h3>
-        <form action="" class="box">
+        <form action="" class="box" method="POST">
             <div class="columns">
                 <div class="column">
                     <div class="field">
                         <label for="nom" class="label">Nom</label>
                         <p class="control is-expanded has-icons-left">
-                            <input class="input" type="text" placeholder="Nom" id="nom" name="nom">
+                            <input class="input" type="text" placeholder="Nom" id="nom" name="nom" required>
                             <span class="icon is-small is-left">
                                 <i class="fas fa-user"></i>
                             </span>
@@ -31,7 +41,8 @@ if ($_SESSION['profile'] == 'Administrateur') {
                     <div class="field">
                         <label for="postnom" class="label">Post-Nom</label>
                         <p class="control is-expanded has-icons-left">
-                            <input class="input" type="text" placeholder="Post-Nom" id="postnom" name="postnom">
+                            <input class="input" type="text" placeholder="Post-Nom" id="postnom" name="postnom"
+                                required>
                             <span class="icon is-small is-left">
                                 <i class="fas fa-user"></i>
                             </span>
@@ -40,9 +51,9 @@ if ($_SESSION['profile'] == 'Administrateur') {
                 </div>
                 <div class="column">
                     <div class="field">
-                        <label for="prenom" class="label">Nom</label>
+                        <label for="prenom" class="label">Prénom</label>
                         <p class="control is-expanded has-icons-left">
-                            <input class="input" type="text" placeholder="Prénom" id="prenom" name="prenom">
+                            <input class="input" type="text" placeholder="Prénom" id="prenom" name="prenom" required>
                             <span class="icon is-small is-left">
                                 <i class="fas fa-user"></i>
                             </span>
@@ -55,7 +66,8 @@ if ($_SESSION['profile'] == 'Administrateur') {
                     <div class="field">
                         <label for="email" class="label">Email</label>
                         <p class="control is-expanded has-icons-left">
-                            <input type="email" name="email" id="email" class="input" placeholder="lorem@exemple.com">
+                            <input type="email" name="email" id="email" class="input" placeholder="lorem@exemple.com"
+                                required>
                             <span class="icon is-small is-left">
                                 <i class="fa-solid fa-envelope"></i>
                             </span>
@@ -83,15 +95,15 @@ if ($_SESSION['profile'] == 'Administrateur') {
                     <div class="field">
                         <label for="phone_1" class="label">N° Téléphone</label>
                         <p class="control is-expanded has-icons-left">
-                            <input type="tel" name="phone_1" id="phone_1" class="input"
-                                placeholder="Numéro de téléphone">
+                            <input type="tel" name="telephone_1" id="phone_1" class="input"
+                                placeholder="Numéro de téléphone" required>
                             <span class="icon is-small is-left">
                                 <i class="fa-solid fa-phone"></i>
                             </span>
                         </p>
                     </div>
                 </div>
-                <div class="column">
+                <!--<div class="column">
                     <div class="field">
                         <label for="phone_2" class="label">N° Téléphone</label>
                         <p class="control is-expanded has-icons-left">
@@ -114,7 +126,7 @@ if ($_SESSION['profile'] == 'Administrateur') {
                             </span>
                         </p>
                     </div>
-                </div>
+                </div>-->
             </div>
             <div class="columns">
                 <div class="column">
@@ -122,7 +134,7 @@ if ($_SESSION['profile'] == 'Administrateur') {
                         <label for="lieu_naissance" class="label">Lieu de naissance</label>
                         <p class="control is-expanded has-icons-left">
                             <input type="text" name="lieu_naissance" id="lieu_naissance" class="input"
-                                placeholder="Lieu de naissance">
+                                placeholder="Lieu de naissance" required>
                         </p>
                     </div>
                 </div>
@@ -130,10 +142,10 @@ if ($_SESSION['profile'] == 'Administrateur') {
                     <div class="field">
                         <label for="date_naissance" class="label">Date de naissance</label>
                         <p class="control is-expanded has-icons-left">
-                            <input type="date" name="date_naissance" id="date_naissance" class="input">
-                                <span class="icon is-small is-left">
-                                    <i class="fa-solid fa-calendar"></i>
-                                </span>
+                            <input type="date" name="date_naissance" id="date_naissance" class="input" required>
+                            <span class="icon is-small is-left">
+                                <i class="fa-solid fa-calendar"></i>
+                            </span>
                         </p>
                     </div>
                 </div>
@@ -142,10 +154,10 @@ if ($_SESSION['profile'] == 'Administrateur') {
                         <label for="nationalite" class="label">Nationalité</label>
                         <p class="control is-expanded has-icons-left">
                             <input type="text" name="nationalite" id="nationalite" class="input"
-                                placeholder="Nationalité">
-                                <span class="icon is-small is-left">
-                                    <i class="fa-solid fa-location-dot"></i>
-                                </span>
+                                placeholder="Nationalité" required>
+                            <span class="icon is-small is-left">
+                                <i class="fa-solid fa-location-dot"></i>
+                            </span>
                         </p>
                     </div>
                 </div>
@@ -155,8 +167,8 @@ if ($_SESSION['profile'] == 'Administrateur') {
                     <div class="field">
                         <label for="profession" class="label">Profession</label>
                         <p class="control is-expanded has-icons-left">
-                            <input type="text" name="profession" id="profession" class="input"
-                                placeholder="Profession">
+                            <input type="text" name="profession" id="profession" class="input" placeholder="Profession"
+                                required>
                             <span class="icon is-small is-left">
                                 <i class="fa-solid fa-user-tie"></i>
                             </span>
@@ -168,8 +180,8 @@ if ($_SESSION['profile'] == 'Administrateur') {
                         <label for="parti" class="label">Parti politique</label>
                         <p class="control is-expanded has-icons-left">
                             <input type="text" name="parti" id="parti" class="input"
-                                placeholder="Parti politique">
-                            <span class="icon is-small is-left">
+                                placeholder="Parti politique ou Indépendant">
+                            <span class="icon is-small is-left" required>
                                 <i class="fa-solid fa-people-group"></i>
                             </span>
                         </p>
@@ -181,8 +193,7 @@ if ($_SESSION['profile'] == 'Administrateur') {
                     <div class="field">
                         <label for="rue" class="label">Rue</label>
                         <p class="control is-expanded has-icons-left">
-                            <input type="text" name="rue" id="rue" class="input"
-                                placeholder="Rue (Avenue)">
+                            <input type="text" name="rue" id="rue" class="input" placeholder="Rue (Avenue)" required>
                             <span class="icon is-small is-left">
                                 <i class="fa-solid fa-home"></i>
                             </span>
@@ -193,8 +204,7 @@ if ($_SESSION['profile'] == 'Administrateur') {
                     <div class="field">
                         <label for="numero" class="label">Numéro</label>
                         <p class="control is-expanded has-icons-left">
-                            <input type="text" name="numero" id="numero" class="input"
-                                placeholder="Numéro">
+                            <input type="text" name="numero" id="numero" class="input" placeholder="Numéro" required>
                             <span class="icon is-small is-left">
                                 <i class="fa-solid fa-hashtag"></i>
                             </span>
@@ -205,8 +215,8 @@ if ($_SESSION['profile'] == 'Administrateur') {
                     <div class="field">
                         <label for="quartier" class="label">Quartier</label>
                         <p class="control is-expanded has-icons-left">
-                            <input type="text" name="quartier" id="quartier" class="input"
-                                placeholder="Quartier">
+                            <input type="text" name="quartier" id="quartier" class="input" placeholder="Quartier"
+                                required>
                             <span class="icon is-small is-left">
                                 <i class="fa-solid fa-map-location"></i>
                             </span>
@@ -217,8 +227,7 @@ if ($_SESSION['profile'] == 'Administrateur') {
                     <div class="field">
                         <label for="commune" class="label">Commune</label>
                         <p class="control is-expanded has-icons-left">
-                            <input type="text" name="commune" id="commune" class="input"
-                                placeholder="Commune">
+                            <input type="text" name="commune" id="commune" class="input" placeholder="Commune" required>
                             <span class="icon is-small is-left">
                                 <i class="fa-solid fa-map-location-dot"></i>
                             </span>
@@ -229,10 +238,10 @@ if ($_SESSION['profile'] == 'Administrateur') {
             <div class="columns">
                 <div class="column">
                     <div class="field">
-                        <label for="ville" class="label">Ville</label>
+                        <label for="ville" class="label">Ville de résidence</label>
                         <p class="control is-expanded has-icons-left">
-                            <input type="text" name="ville" id="ville" class="input"
-                                placeholder="Ville de résidence">
+                            <input type="text" name="ville" id="ville" class="input" placeholder="Ville de résidence"
+                                required>
                             <span class="icon is-small is-left">
                                 <i class="fa-solid fa-city"></i>
                             </span>
@@ -241,12 +250,20 @@ if ($_SESSION['profile'] == 'Administrateur') {
                 </div>
                 <div class="column">
                     <div class="field">
-                        <label for="pays" class="label">Pays</label>
-                        <p class="control is-expanded has-icons-left">
-                            <input type="text" name="pays" id="pays" class="input"
-                                placeholder="Pays de résidence">
+                        <label for="pays" class="label">Pays de résidence</label>
+                        <p class="control has-icons-left">
+                            <span class="select">
+                                <select name="pays" id="pays">
+                                    <option value="" selected>Pays--------------</option>
+                                    <?php while($rows = $rep->fetch()):; ?>
+                                    <option value="<?php echo $rows[5]; ?>">
+                                        <?php echo $rows[5]; ?>
+                                    </option>
+                                    <?php endwhile; ?>
+                                </select>
+                            </span>
                             <span class="icon is-small is-left">
-                                <i class="fa-solid fa-globe"></i>
+                                <i class="fas fa-globe"></i>
                             </span>
                         </p>
                     </div>
@@ -254,9 +271,62 @@ if ($_SESSION['profile'] == 'Administrateur') {
             </div><br>
             <div class="columns">
                 <div class="column">
-                    <input type="submit" value="Valider" class="button" style="color:#1C3D59;background-color:#D8E6F2;width:250px">
+                    <input type="submit" value="Valider" class="button"
+                        style="color:#1C3D59;background-color:#D8E6F2;width:250px">
                 </div>
             </div>
         </form>
+        <?php
+            $request = "";
+            /*if(isset($_POST['telephone_2']) or isset($_POST['telephone_3'])){
+            $request = "insert into candidat(nom,postnom,prenom,email,telephone_1,telephone_2,telephone_3,sexe,lieu_de_naissance,date_de_naissance,nationalite,profession,parti_politique,rue,numero,quartier,commune,ville,pays) values (:nom,:postnom,:prenom,:email,:tel1,:tel2,:tel3,:sexe,:lieu_naiss,:date_naiss,:nationalite,:profession,:parti,:rue,:numero,:quartier,:commune,:ville,:pays)";
+            } 
+            else{
+            
+            }*/
+            $request = "insert into candidat(nom,postnom,prenom,email,telephone_1,sexe,lieu_de_naissance,date_de_naissance,nationalite,profession,parti_politique,rue,numero,quartier,commune,ville,pays) values (:nom,:postnom,:prenom,:email,:tel,:sexe,:lieu_naiss,:date_naiss,:nationalite,:profession,:parti,:rue,:numero,:quartier,:commune,:ville,:pays)";
+            $insert=$db->prepare($request);
+            if(!isset($_POST['nom']) && !isset($_POST['postnom']) && !isset($_POST['prenom']) && !isset($_POST['email']) && !isset($_POST['telephone_1']) && !isset($_POST['sexe']) && !isset($_POST['lieu_naissance']) && !isset($_POST['date_naissance']) && !isset($_POST['nationalite']) && !isset($_POST['profession']) && !isset($_POST['parti']) && !isset($_POST['rue']) && !isset($_POST['numero']) && !isset($_POST['quartier']) && !isset($_POST['commune']) && !isset($_POST['ville']) && !isset($_POST['pays'])){
+                echo '';
+            }
+            else{
+                $insert->execute(
+                    array(
+                        'nom' => $_POST['nom'],
+                        'postnom' => $_POST['postnom'],
+                        'prenom' => $_POST['prenom'],
+                        'email' => $_POST['email'],
+                        'tel' => $_POST['telephone_1'],
+                        'sexe' => $_POST['sexe'],
+                        'lieu_naiss' => $_POST['lieu_naissance'],
+                        'date_naiss' => $_POST['date_naissance'],
+                        'nationalite' => $_POST['nationalite'],
+                        'profession' => $_POST['profession'],
+                        'parti' => $_POST['parti'],
+                        'rue' => $_POST['rue'],
+                        'numero' => $_POST['numero'],
+                        'quartier' => $_POST['quartier'],
+                        'commune' => $_POST['commune'],
+                        'ville' => $_POST['ville'],
+                        'pays' => $_POST['pays'],
+                    )
+                    );
+                
+        }
+
+        if(strlen($error_message) > 0){?>
+        <div class="notification is-danger is-light">
+            <h2 class="title"><strong><i class="fa-solid fa-circle-exclamation"></i> Attention !</strong></h2>
+            <?php echo $error_message; ?>
+        </div>
+        <?php }
+
+        if(strlen($success_message) > 0){?>
+        <div class="notification is-success is-light">
+            <h2 class="title"><strong><i class="fa-solid fa-circle-exclamation"></i> Attention !</strong></h2>
+            <?php echo $success_message; ?>
+        </div>
+        <?php }
+        ?>
     </div>
 </body>
